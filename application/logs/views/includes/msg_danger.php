abc01ef5daf4d4670c3c02f798120178636d77bf
Thiago Machado <thiago@iMac-de-Thiago.local>
2019-03-27 23:08:50 -0300

    v1

--- application/logs/views/includes/msg_danger.php
+++
@@ -1,4 +0,0 @@
-<div class="alert alert-danger alert-dismissible" style=" width: 83%;  margin: 0 0 0 230px;" role="alert">
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-    <?= $msg?>
-</div>
