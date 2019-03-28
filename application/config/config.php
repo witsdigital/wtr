581f73c92ff61cc8305d1882ad37a7973e4ff69d
Thiago Machado <thiago@iMac-de-Thiago.local>
2019-03-27 23:14:21 -0300

    v

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@
 | a PHP script and you can easily do that on your own.
 | 
 */
-$config['base_url'] = 'https://pocoes.ba.gov.br/';
+$config['base_url'] = 'https://transparencia.pocoes.ba.gov.br/';
 
 /* 
 |--------------------------------------------------------------------------
